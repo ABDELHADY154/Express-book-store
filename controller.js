ceb6de6faff024db6203fed08235a8e12043bac8
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2021-05-06 04:39:24 +0200

    ..

--- controller.js
+++ controller.js
@@ -1,5 +1,127 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const db = require("./myDb.js");
+var bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+var fs = require("fs");
+app.use(cookieParser());
+bodyParser = bodyParser.urlencoded({ extended: false });
+
+var users = [];
+fs.readFile("users.json", function (err, data) {
+  if (err) return console.log(err);
+  if (data) {
+    users = JSON.parse(data);
+    // users.push(JSON.parse(data));
+  }
+});
+
+// Landing Page
+exports.landingComponent = (req, res) => {
+  res.layout("landing", {
+    layout: "index",
+    loggedIn: false,
+  });
+};
+
+// Authentication
+exports.loginComponent = (req, res) => {
+  res.layout("login", {
+    layout: "index",
+    error: "",
+  });
+};
+
+exports.registerComponent = (req, res) => {
+  res.layout("register", {
+    layout: "index",
+    error: "",
+    loggedIn: false,
+  });
+};
+
+exports.registerForm = (req, res, loggedIn) => {
+  if (req.body.fullname && req.body.email && req.body.password) {
+    res.cookie("didlogin", "true");
+    var user = {
+      fullName: req.body.fullname,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    users.forEach(data => {
+      if (data.email == user.email) {
+        res.layout("register", {
+          layout: "index",
+          error: "Email is already Taken",
+        });
+      }
+    });
+    res.cookie("userData", user);
+    users.push(user);
+    fs.writeFile("users.json", JSON.stringify(users), function (err) {
+      if (err) return console.log(err);
+    });
+    loggedIn = true;
+    // res.layout("home", {
+    //   layout: "index",
+    //   loggedIn: true,
+    // });
+    res.redirect("/home");
+  } else {
+    res.layout("register", {
+      layout: "index",
+      error: "enter a valid data",
+      loggedIn: false,
+    });
+  }
+};
+
+exports.logout = (req, res) => {
+  res.cookie("didlogin", "false");
+  res.clearCookie("userData");
+  res.redirect("/");
+};
+
+exports.loginForm = (req, res) => {
+  if (req.body.email && req.body.password) {
+    // res.cookie("didlogin", "true");
+    var user = {
+      // fullName: req.body.fullname,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    users.forEach(data => {
+      if (data.email == user.email && data.password == user.password) {
+        res.cookie("didlogin", "true");
+        res.cookie("userData", user);
+        res.redirect("/home");
+      }
+    });
+    res.layout("login", {
+      layout: "index",
+      error: "incorrect email or password",
+      loggedIn: false,
+    });
+  } else {
+    res.layout("login", {
+      layout: "index",
+      error: "enter a valid data",
+      loggedIn: false,
+    });
+  }
+};
+
+exports.homeComponent = (req, res) => {
+  var books = [];
+  fs.readFile("book-data.db", function (err, data) {
+    if (err) return console.log(err);
+    if (data) {
+      books = JSON.parse(data);
+
+      res.layout("home", {
+        layout: "authIndex",
+        books: books,
+      });
+      // users.push(JSON.parse(data));
+    }
+  });
+};
--- server.js
+++ server.js
@@ -2,16 +2,67 @@ const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-// const controller = require("./controller");
+const controller = require("./controller");
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(require("ejs-yield"));
 bodyParser = bodyParser.urlencoded({ extended: false });
+
 app.get("/", (req, res) => {
-  // res.send("welcome");
-  res.layout("login", {
-    layout: "index",
-  });
+  if (req.cookies.didlogin == "true") {
+    res.redirect("/home");
+  } else {
+    controller.landingComponent(req, res);
+  }
 });
 
+app
+  .route("/login")
+  .get((req, res) => {
+    if (req.cookies.didlogin == "true") {
+      res.redirect("/home");
+    } else {
+      controller.loginComponent(req, res);
+    }
+  })
+  .post(bodyParser, (req, res) => {
+    if (req.cookies.didlogin == "true") {
+      res.redirect("/home");
+    } else {
+      controller.loginForm(req, res);
+    }
+  });
+
+app
+  .route("/register")
+  .get((req, res) => {
+    if (req.cookies.didlogin == "true") {
+      res.redirect("/home");
+    } else {
+      controller.registerComponent(req, res);
+    }
+  })
+  .post(bodyParser, (req, res) => {
+    if (req.cookies.didlogin == "true") {
+      res.redirect("/home");
+    } else {
+      controller.registerForm(req, res);
+    }
+  });
+
+app.get("/home", (req, res) => {
+  if (req.cookies.didlogin == "true") {
+    controller.homeComponent(req, res);
+  } else {
+    res.redirect("/");
+  }
+});
+app.get("/logout", (req, res) => {
+  controller.logout(req, res);
+});
+
+// fallback route
+app.get("*", function (req, res) {
+  res.send("page not found");
+});
 app.listen(5000);
